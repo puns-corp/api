b3d50f0682ff76dbb0584c313252ca8cf6e3dda5
Piotr Radlak <p10trek@o2.pl>
2020-11-08 14:02:46 +0100

    PUNS-15 dodano strukture bazo danową

---
+++ Controllers/PlayersController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PunsApi.Models;
+
+namespace PunsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PlayersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Players
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Player>>> GetPlayer()
+        {
+            return await _context.Player.ToListAsync();
+        }
+
+        // GET: api/Players/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Player>> GetPlayer(Guid id)
+        {
+            var player = await _context.Player.FindAsync(id);
+
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return player;
+        }
+
+        // PUT: api/Players/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPlayer(Guid id, Player player)
+        {
+            if (id != player.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(player).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlayerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Players
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Player>> PostPlayer(Player player)
+        {
+            _context.Player.Add(player);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPlayer", new { id = player.ID }, player);
+        }
+
+        // DELETE: api/Players/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Player>> DeletePlayer(Guid id)
+        {
+            var player = await _context.Player.FindAsync(id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            _context.Player.Remove(player);
+            await _context.SaveChangesAsync();
+
+            return player;
+        }
+
+        private bool PlayerExists(Guid id)
+        {
+            return _context.Player.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Models/Results.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PunsApi.Models
+{
+    public class Results
+    {
+        public Guid Id { get; set;}
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        [ForeignKey("Player")]
+        public virtual Player Player { get; set; }
+        public int Score { get; set; }
+    }
+}
