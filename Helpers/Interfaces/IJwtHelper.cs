6d7af88f15d7a741f74ecbcf2df08880b9d2fc09
Marcin <ziewa91@wp.pl>
2020-11-21 14:42:25 +0100

    Added JwtHelper

---
+++ Helpers/Interfaces/IJwtHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PunsApi.Models;
+
+namespace PunsApi.Helpers.Interfaces
+{
+    public interface IJwtHelper
+    {
+        string GenerateJwtToken(Player player);
+
+        RefreshToken GenerateRefreshToken();
+    }
+}
