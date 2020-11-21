68b77343cc1377cd97c9cb786c6655bc1e91fd0f
Marcin <ziewa91@wp.pl>
2020-11-21 14:43:42 +0100

    Added AuthenticationViewModel

---
+++ ViewModels/Authenticate/AuthenticateViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PunsApi.Models;
+
+namespace PunsApi.ViewModels.Authenticate
+{
+    public class AuthenticateViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Nick { get; set; }
+
+        public string AccessToken { get; set; }
+
+        public string RefreshToken { get; set; }
+
+        public AuthenticateViewModel(Player player, string jwtToken, string refreshToken)
+        {
+            Id = player.Id;
+            Nick = player.Nick;
+            AccessToken = jwtToken;
+            RefreshToken = refreshToken;
+        }
+    }
+}
