b513603edf0b6c8dac0856e382b0d657032e73e5
Marcin <ziewa91@wp.pl>
2020-11-21 14:44:16 +0100

    Added implementation for login, refresh token and revoke token

--- Services/AuthenticationService.cs
+++ Services/AuthenticationService.cs
@@ -7,10 +7,12 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PunsApi.Data;
 using PunsApi.Helpers;
+using PunsApi.Helpers.Interfaces;
 using PunsApi.Models;
 using PunsApi.Requests.Authentication;
 using PunsApi.Services.Interfaces;
 using PunsApi.Services.ServicesResponses;
+using PunsApi.ViewModels.Authenticate;
 
 namespace PunsApi.Services
 {
@@ -18,13 +20,15 @@ namespace PunsApi.Services
     {
         private readonly PasswordHasher<Player> _passwordHasher;
         private readonly PlayerPasswordValidator _passwordValidator;
+        private readonly IJwtHelper _jwtHelper;
 
 
         public AuthenticationService(AppDbContext context, PasswordHasher<Player> passwordHasher,
-            PlayerPasswordValidator passwordValidator) : base(context)
+            PlayerPasswordValidator passwordValidator, IJwtHelper jwtHelper) : base(context)
         {
             _passwordHasher = passwordHasher;
             _passwordValidator = passwordValidator;
+            _jwtHelper = jwtHelper;
         }
 
         public async Task<ServiceResponse<bool>> Register(AuthenticateRequest request)
@@ -60,19 +64,77 @@ namespace PunsApi.Services
             return ServiceResponse<bool>.Ok(true, "User created");
         }
 
-        public Task<ServiceResponse<bool>> Login()
+        public async Task<ServiceResponse<AuthenticateViewModel>> Login(AuthenticateRequest request)
         {
-            throw new NotImplementedException();
+            var player = await _context.Players.Include(
+                x => x.RefreshToken).FirstOrDefaultAsync(
+                x => x.Email == request.Email);
+
+            if (player == null)
+                return ServiceResponse<AuthenticateViewModel>.Error("User for this email does not exist");
+
+            var verifyPasswordResult = _passwordHasher.VerifyHashedPassword(player, player.PasswordHash, request.Password);
+
+            if (verifyPasswordResult != PasswordVerificationResult.Success)
+                return ServiceResponse<AuthenticateViewModel>.Error("Invalid password");
+
+            var jwtToken = _jwtHelper.GenerateJwtToken(player);
+
+            var refreshToken = _jwtHelper.GenerateRefreshToken();
+
+            player.RefreshToken = refreshToken;
+            _context.Update(player);
+            await _context.SaveChangesAsync();
+
+            return ServiceResponse<AuthenticateViewModel>.Ok(
+                new AuthenticateViewModel(player, jwtToken, refreshToken.Token));
         }
 
-        public Task<ServiceResponse<bool>> RefreshToken()
+        public async Task<ServiceResponse<AuthenticateViewModel>> RefreshToken(RefreshTokenRequest request)
         {
-            throw new NotImplementedException();
+            var user = await _context.Players.Include(
+                x => x.RefreshToken).FirstOrDefaultAsync(
+                u => u.RefreshToken.Token == request.RefreshToken);
+
+            if (user == null)
+                return ServiceResponse<AuthenticateViewModel>.Error("Invalid refresh token");
+
+            if (!user.RefreshToken.IsActive)
+                return ServiceResponse<AuthenticateViewModel>.Error("Refresh token is not active");
+
+            var newRefreshToken = _jwtHelper.GenerateRefreshToken();
+            user.RefreshToken = newRefreshToken;
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+
+            var jwtToken = _jwtHelper.GenerateJwtToken(user);
+
+            return ServiceResponse<AuthenticateViewModel>.Ok(new AuthenticateViewModel(user, jwtToken, newRefreshToken.Token));
         }
 
-        public Task<ServiceResponse<bool>> RevokeToken()
+        public async Task<ServiceResponse<bool>> RevokeToken(RefreshTokenRequest request)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(request.RefreshToken))
+                return ServiceResponse<bool>.Error("Refresh token is required");
+
+            var player = 
+                await _context.Players.Include(
+                    x => x.RefreshToken).FirstOrDefaultAsync(
+                    u => u.RefreshToken.Token == request.RefreshToken);
+
+            if (player == null)
+                return ServiceResponse<bool>.Error("Invalid refresh token");
+
+            var refreshToken = player.RefreshToken;
+
+            if (!refreshToken.IsActive)
+                return ServiceResponse<bool>.Error("Refresh token is not active");
+
+            refreshToken.Revoked = true;
+            _context.Update(player);
+            await _context.SaveChangesAsync();
+
+            return ServiceResponse<bool>.Ok(true, "Token revoked");
         }
     }
 }
--- Services/Interfaces/IAuthenticationService.cs
+++ Services/Interfaces/IAuthenticationService.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using PunsApi.Requests.Authentication;
 using PunsApi.Services.ServicesResponses;
+using PunsApi.ViewModels.Authenticate;
 
 namespace PunsApi.Services.Interfaces
 {
     public interface IAuthenticationService
     {
         Task<ServiceResponse<bool>> Register(AuthenticateRequest request);
-        Task<ServiceResponse<bool>> Login();
-        Task<ServiceResponse<bool>> RefreshToken();
-        Task<ServiceResponse<bool>> RevokeToken();
+
+        Task<ServiceResponse<AuthenticateViewModel>> Login(AuthenticateRequest request);
+
+        Task<ServiceResponse<AuthenticateViewModel>> RefreshToken(RefreshTokenRequest request);
+
+        Task<ServiceResponse<bool>> RevokeToken(RefreshTokenRequest request);
     }
 }
