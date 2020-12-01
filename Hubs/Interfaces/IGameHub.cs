6095f3c09b0881174351f1ee726c7f611e5bff8d
Marcin <ziewa91@wp.pl>
2020-12-01 15:45:26 +0100

    GameHub implementation

---
+++ Hubs/Interfaces/IGameHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PunsApi.Hubs.Interfaces
+{
+    public interface IGameHub
+    {
+        Task PlayerJoined(string playerName);
+
+        Task PlayerQuit(string playerName);
+
+        Task GameStarted();
+
+        Task SendMessageToTest(string message);
+    }
+}
