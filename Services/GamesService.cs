83b540ad4abf4953ff03be36721c9627d9ce1632
Marcin <ziewa91@wp.pl>
2020-12-01 15:49:33 +0100

    Refactoring

--- Services/GamesService.cs
+++ Services/GamesService.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using PunsApi.Data;
 using PunsApi.Helpers;
+using PunsApi.Hubs;
+using PunsApi.Hubs.Interfaces;
 using PunsApi.Models;
 using PunsApi.Requests.Games;
 using PunsApi.Requests.Rooms;
@@ -17,13 +20,16 @@ namespace PunsApi.Services
 {
     public class GamesService : BaseService, IGamesService
     {
-        public GamesService(AppDbContext context, IHttpContextAccessor httpContextAccessor) : base(context, httpContextAccessor)
+        private readonly IHubContext<GameHub, IGameHub> _gameHubContext;
+
+        public GamesService(AppDbContext context, IHttpContextAccessor httpContextAccessor, IHubContext<GameHub, IGameHub> gameHubContext) : base(context, httpContextAccessor)
         {
+            _gameHubContext = gameHubContext;
         }
 
         public async Task<ServiceResponse<bool>> CreateGame(CreateGameRequest request)
         {
-            var player = await GetCurrentPlayer();
+            var player = await GetPlayer();
 
             if (player == null)
                 return ServiceResponse<bool>.Error("No player found");
@@ -33,7 +39,7 @@ namespace PunsApi.Services
             if(room == null)
                 return ServiceResponse<bool>.Error("No room found");
 
-            var isRoomNameValid = NamesValidator.IsRoomNameValid(request.GameName);
+            var isRoomNameValid = NamesValidator.IsNameValid(request.GameName);
 
             if (!isRoomNameValid)
                 return ServiceResponse<bool>.Error("Game name is too short");
@@ -48,6 +54,7 @@ namespace PunsApi.Services
             await _context.Games.AddAsync(newGame);
             await _context.SaveChangesAsync();
             player.GameId = newGame.Id;
+            player.IsGameMaster = true;
             _context.Update(player);
             await _context.SaveChangesAsync();
 
@@ -56,56 +63,75 @@ namespace PunsApi.Services
 
         public async Task<ServiceResponse<bool>> JoinGame(string gameId)
         {
-            var player = await GetCurrentPlayer();
+            var (response, player) = await ValidateRequest(gameId, true);
 
-            if (player == null)
-                return ServiceResponse<bool>.Error("No player found");
+            if (!response.Success)
+                return response;
 
-            var game = await _context.Games.FirstOrDefaultAsync(x => x.Id.ToString() == gameId);
-
-            if(game == null)
-                return ServiceResponse<bool>.Error("No game found");
-
-            var room = await _context.Rooms.FirstOrDefaultAsync(x => x.Id == player.RoomId);
-
-            if (room == null)
-                return ServiceResponse<bool>.Error("No room found");
-
-            if(room.Id != game.RoomId)
-                return ServiceResponse<bool>.Error("User isn't in this room");
-
-            player.GameId = game.Id;
+            player.GameId = player.Game.Id;
             _context.Update(player);
             await _context.SaveChangesAsync();
+
+            await _gameHubContext.Clients.Group(gameId).PlayerJoined(player.Nick);
 
             return ServiceResponse<bool>.Ok(true, "Player joined to game");
         }
 
         public async Task<ServiceResponse<bool>> QuitGame(string gameId)
         {
-            var player = await GetCurrentPlayer();
+            var (response, player) = await ValidateRequest(gameId, true);
 
-            if (player == null)
-                return ServiceResponse<bool>.Error("No player found");
+            if (!response.Success)
+                return response;
 
-            var game = await _context.Games.FirstOrDefaultAsync(x => x.Id.ToString() == gameId);
-
-            if (game == null)
-                return ServiceResponse<bool>.Error("No game found");
-
-            var room = await _context.Rooms.FirstOrDefaultAsync(x => x.Id == player.RoomId);
-
-            if (room == null)
-                return ServiceResponse<bool>.Error("No room found");
-
-            if (room.Id != game.RoomId)
+            if (player.Room.Id != player.Game.RoomId)
                 return ServiceResponse<bool>.Error("User isn't in this room");
 
             player.GameId = null;
             _context.Update(player);
             await _context.SaveChangesAsync();
 
+            await _gameHubContext.Clients.Group(gameId).PlayerQuit(player.Nick);
+
             return ServiceResponse<bool>.Ok(true, "Player quit game");
+        }
+
+        public async Task<ServiceResponse<bool>> StartGame(string gameId)
+        {
+            var (response, player) = await ValidateRequest(gameId, true);
+
+            if (!response.Success)
+                return response;
+
+            if (!player.IsGameMaster)
+                return ServiceResponse<bool>.Error("Player isn't game master");
+
+            await _gameHubContext.Clients.Group(gameId).GameStarted();
+
+            return ServiceResponse<bool>.Ok(true, "Game started");
+        }
+
+        private async Task<(ServiceResponse<T>, Player)> ValidateRequest<T>(string gameId, T responseType)
+        {
+            var player = await GetPlayer();
+
+            if (player == null)
+                return (ServiceResponse<T>.Error("No player found"), null);
+
+            var game = await _context.Games.FirstOrDefaultAsync(x => x.Id.ToString() == gameId);
+
+            if (game == null)
+                return (ServiceResponse<T>.Error("No game found"), null);
+
+            var room = await _context.Rooms.FirstOrDefaultAsync(x => x.Id == player.RoomId);
+
+            if (room == null)
+                return (ServiceResponse<T>.Error("No room found"), null);
+
+            player.Room = room;
+            player.Game = game;
+
+            return (ServiceResponse<T>.Ok(responseType, "Validate "), player);
         }
     }
 }
