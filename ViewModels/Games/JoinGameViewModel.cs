4178a754b02cec2d1e54283ccf2f9b5183b62484
Marcin <ziewa91@wp.pl>
2020-12-07 16:52:27 +0100

    Implementation JoinGameViewModel

---
+++ ViewModels/Games/JoinGameViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PunsApi.Models;
+
+namespace PunsApi.ViewModels.Games
+{
+    public class JoinGameViewModel
+    {
+        public string GameId { get; set; }
+
+        public string GameMasterId { get; set; }
+
+        public List<Player> Players { get; set; }
+
+        public JoinGameViewModel(string gameId, string gameMasterId, List<Player> players)
+        {
+            GameId = gameId;
+            GameMasterId = gameMasterId;
+            Players = players;
+        }
+    }
+}
